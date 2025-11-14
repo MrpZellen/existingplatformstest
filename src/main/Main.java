66809767cb00be51b251016890bb113cb9df71a4
MrpZellen <158220937+MrpZellen@users.noreply.github.com>
2025-11-14 15:12:35 -0700

    Menu added at start of application

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Main {
 
@@ -12,17 +14,42 @@ public class Main {
         JFrame window = new JFrame("Chess Game");
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
+        JPanel panel = new JPanel();
+        JButton mainGame = new JButton("Regular Chess");
+        JButton game960 = new JButton("960 Chess");
+
+        GamePanel gp = new GamePanel();
+        Game960Panel g960p = new Game960Panel();
+        mainGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window.getContentPane().removeAll();
+                window.add(gp);
+                window.pack();
+                window.setVisible(true);
+            }
+        });
+
+        game960.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window.getContentPane().removeAll();
+                window.add(g960p);
+                window.pack();
+                window.setVisible(true);
+            }
+        });
+
+
+        panel.add(mainGame);
+        panel.add(game960);
 //        window.getIconImage(logo.getImage());
 
         //Add Main.Main.GamePanel to the window
-        GamePanel gp = new GamePanel();
-        Game960Panel g960p = new Game960Panel();
-        window.add(g960p);
+        window.add(panel);
         window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
-
-        gp.lauchGame();
     }
 }
